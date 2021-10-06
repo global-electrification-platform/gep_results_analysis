944149380da51a1eb857a8c2175b75451b9fc17c
bpstewar <ben.gis.stewart@gmail.com>
2021-10-06 10:30:26 -0400

    updated analysis to work with delivery

--- GEP_analysis/GEP.py
+++ GEP_analysis/GEP.py
@@ -14,13 +14,20 @@ from rasterio.features import rasterize
 
 
 scenario_defs = {
-               0:[["Medium","High"], "Population growth"],
-               1:[["Top-down low","Top-down high", "bottom up"], "Electricity demand target"],
-               2:[["No connections cap","Capped connections in 2025"], "Intermediate investment"],
-               3:[["Estimated","High"], "Grid generation cost"],
-               4:[["Estimated","High", "Low"], "PV cost"],
-               5:[["Least-cost nationwide","Only grid within 1 km","Only grid within 2 km"], "Rollout"],
+               0:[["Bottom-up","Top-down low", "Top-down high"], "Electricity demand target"],
+               1:[["Productive included", "Residential only"], "Social & commercial uses"],
+               2:[["Estimated","High"], "Grid generation cost"],
+               3:[["Estimated","Low"], "PV cost"],
+               4:[["No connections cap","Capped connections in 2025"], "Intermediate investment"],
+               5:[["Least-cost nationwide","Grid within 2 km"], "Rollout"],
                }
+
+summary_defs = {
+	1:'Population',
+	2:'New connections',
+	3:'Capacity added',
+	4:'Investment required'
+}
 
 def get_assumptions(input_scenario):
     """ Convert the six digit scenario into a list of assumptions
@@ -39,10 +46,10 @@ def get_assumptions(input_scenario):
 def get_continent(x):
     ''' classify power pools - used in pandas apply function
     '''
-    EAPP = [f'{x}-1' for x in ['bi','dj','et','ke','ly','rw','sd','ss','ug']]
-    WAPP = [f'{x}-1' for x in ['bf','bj','ci','gh','gm','gn','gw','lr','ml','ng','sl','sn','tg']]
-    SAPP = [f'{x}-1' for x in ['ao','bw','ls','mw','mz','na','sz','tz','za','zm','zw']]
-    AFR =  [f'{x}-1' for x in ['cd','cg','cf','cm','er','ga','gq','km','mg','mr','ne','so','st','td']]
+    EAPP = [f'{x}' for x in ['bi','dj','et','ke','ly','rw','sd','ss','ug']]
+    WAPP = [f'{x}' for x in ['bf','bj','ci','gh','gm','gn','gw','lr','ml','ng','sl','sn','tg']]
+    SAPP = [f'{x}' for x in ['ao','bw','ls','mw','mz','na','sz','tz','za','zm','zw']]
+    AFR =  [f'{x}' for x in ['cd','cg','cf','cm','er','ga','gq','km','mg','mr','ne','so','st','td']]
     if x in EAPP:
         return('AFR')
     if x in WAPP:
@@ -57,10 +64,10 @@ def get_continent(x):
 def get_pp(x):
     ''' classify power pools - used in pandas apply function
     '''
-    EAPP = [f'{x}-1' for x in ['bi','dj','et','ke','ly','rw','sd','ss','ug']]
-    WAPP = [f'{x}-1' for x in ['bf','bj','ci','gh','gm','gn','gw','lr','ml','ng','sl','sn','tg']]
-    SAPP = [f'{x}-1' for x in ['ao','bw','ls','mw','mz','na','sz','tz','za','zm','zw']]
-    AFR = [f'{x}-1' for x in ['cd','cg','cf','cm','er','ga','gq','km','mg','mr','ne','so','st','td']]
+    EAPP = [f'{x}' for x in ['bi','dj','et','ke','ly','rw','sd','ss','ug']]
+    WAPP = [f'{x}' for x in ['bf','bj','ci','gh','gm','gn','gw','lr','ml','ng','sl','sn','tg']]
+    SAPP = [f'{x}' for x in ['ao','bw','ls','mw','mz','na','sz','tz','za','zm','zw']]
+    AFR = [f'{x}' for x in ['cd','cg','cf','cm','er','ga','gq','km','mg','mr','ne','so','st','td']]
     if x in EAPP:
         return('EAPP')
     if x in WAPP:
@@ -70,6 +77,27 @@ def get_pp(x):
     if x in AFR:
         return('AFR')
     return("other")
+
+
+def get_tech(x):
+    ''' Convert TECH column in GRID, OFF-GRID (SA and MG)
+    '''
+    x_spl = x.split("_")
+    if x_spl[-1] == "Grid":
+        return("GRID")
+    elif "Connections" in x:
+        return("_".join(x_spl[2:]))
+    else:
+        return("_".join(x_spl[1:]))
+
+def get_simple_tech(x):
+    ''' Convert TECH column in GRID, OFF-GRID (SA and MG)
+    '''
+    x_spl = x.split("_")
+    if x_spl[-1] == "Grid":
+        return("GRID")
+    else:
+        return(x_spl[-2])
 
 def box_plot(inD, selected_attribute, out_file):
     data_bad_idx = inD['2025'] == inD['2030']
@@ -132,7 +160,7 @@ class gep_summary(object):
     def __init__(self, file):
         self.file = file
         self.file_name = os.path.basename(file)
-        self.country = self.file_name[:4]
+        self.country = self.file_name[:2]
         self.scenario = self.file_name[5:16]
 
     def get_data(self):
@@ -146,8 +174,8 @@ class gep_summary(object):
 
 class gep_results(object):
     def __init__(self, country,
-                clustersFolder = '/media/gost/DATA/GEP/Clusters',
-                scenariosFolder = '/media/gost/DATA/GEP/Scenarios',
+                clustersFolder = '/media/gost/DATA1/GEP/Clusters',
+                scenariosFolder = '/media/gost/DATA1/GEP/Scenarios',
                 scenario = '0_0_0_0_0_0'):
         ''' Object for extracting and processing GEP results files
 
@@ -157,22 +185,19 @@ class gep_results(object):
         self.country = country
         self.clustersFolder = clustersFolder
         self.clustersZipFile = os.path.join(clustersFolder, f'{country}.zip')
-        self.scenariosZipFile = os.path.join(scenariosFolder, f'{country}-scenarios-results.zip')
+        self.scenariosZipFile = os.path.join(scenariosFolder, 'zipfiles', f'{country}-scenarios-results.zip')
+        # Define clusters file
+        countryClustersFolder = os.path.join(clustersFolder, f'{country.split("-")[0]}-1')
+        for x in os.listdir(countryClustersFolder):
+            if x.endswith(".shp"):
+                self.clustersFile = os.path.join(countryClustersFolder, x)
+        # Define scenario files
+        scenarioFolder = os.path.join(scenariosFolder, scenario)
+        for x in os.listdir(scenarioFolder):
+            if x.startswith(country):
+                self.scenarioFile = os.path.join(scenarioFolder, x)
 
-        #Extract the scenario sample
-        self.sampleScenarioFolder = os.path.join(scenariosFolder, "%s-scenarios-results" % country)
-        if not os.path.exists(self.sampleScenarioFolder):
-            if not os.path.exists(self.scenariosZipFile):
-                raise(ValueError(f"The scenario results for {country} have not been downloaded: {self.sampleScenarioFolder}"))
-            os.makedirs(self.sampleScenarioFolder)
-            extractedFile = self.extract_sample_scenario(f'{country}-{scenario}.csv', self.sampleScenarioFolder)
-            self.sampleScenarioFile = os.path.join(self.sampleScenarioFolder, extractedFile)
-        else:
-            for root, folders, files in os.walk(self.sampleScenarioFolder):
-                for f in files:
-                    if f == f'{country}-{scenario}.csv':
-                        self.sampleScenarioFile = os.path.join(root, f)
-
+    def initiate():
         #Extract the clusters
         self.countryClustersFolder = os.path.join(clustersFolder, country)
         try:
@@ -207,17 +232,26 @@ class gep_results(object):
             for f in allFiles:
                 if scenario in f:
                     zipObj.extract(f, outFolder)
-                    return(f)
+                    return(os.path.join(outFolder, f))
 
 
     def join_results(self):
         ''' read in the scenario results and the clusters and join them
         '''
         inD = gpd.read_file(self.clustersFile)
-        inRes = pd.read_csv(self.sampleScenarioFile)
+        inRes = pd.read_csv(self.scenarioFile)
         for x_col in inD.columns:
             if x_col in inRes.columns and x_col != "id":
                 inD = inD.drop([x_col],axis=1)
+
+        def fix_id(x):
+            return(int(x.replace(",","")))
+
+        #inD['id'] = inD['id'].apply(fix_id)
+        try:
+            inRes['id'] = inRes['id'].apply(fix_id)
+        except:
+            pass
         inCombined = pd.merge(inD, inRes, on='id')
 
         #generate additional columns
